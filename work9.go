12e7bebbba25b53dd4c9452196c9994408a9aebd
star <1768447040@qq.com>
2023-09-03 14:33:40 +0800

    first go work

---
+++ work9.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func generate(ch chan int) {
+	for i := 2; ; i++ {
+		ch <- i
+	}
+}
+
+func filter(in chan int, out chan int, prime int) {
+	for {
+		num := <-in
+		fmt.Println("num=", num)
+		if num%prime != 0 {
+			out <- num
+		}
+	}
+}
+
+func main() {
+	ch := make(chan int)
+	go generate(ch)
+	for i := 0; i < 6; i++ {
+		prime := <-ch
+		fmt.Printf("prime:%d\n", prime)
+		out := make(chan int)
+		go filter(ch, out, prime)
+		ch = out
+		fmt.Println(<-ch)
+	}
+}
